e0753006e437284148752ce739be750109511c2b
tuhincoder <mdtuhinmahmudul@gmail.com>
2024-12-08 15:39:54 +0600

    academic interface and model complete

---
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -0,0 +1,23 @@
+import { model, Schema } from 'mongoose';
+import { TAcademicDepartment } from './academicDepartment.interface';
+
+const academicDepartmentSchema = new Schema<TAcademicDepartment>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    academicFaculty: {
+      type: Schema.Types.ObjectId,
+      ref: 'AcademicDepartment',
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const AcademicDepartment = model<TAcademicDepartment>(
+  'AcademicDepartment',
+  academicDepartmentSchema,
+);
