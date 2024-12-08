4718711dab8f137280833b7afae21057692e820e
tuhincoder <mdtuhinmahmudul@gmail.com>
2024-12-08 18:08:09 +0600

    academic department validation

---
+++ src/app/modules/academicDepartment/academicDepartment.validation.ts
@@ -0,0 +1,33 @@
+import { z } from 'zod';
+
+//create academic department validation schema
+const createAcademicDepartmentValidationSchema = z.object({
+  body: z.object({
+    name: z.string({
+      invalid_type_error: 'academic Department must be string',
+      required_error: 'Name is required',
+    }),
+    academicFaculty: z.string({
+      invalid_type_error: 'Academic Faculty is must be string',
+      required_error: 'Academic faculty is required',
+    }),
+  }),
+});
+
+const updateAcademicDepartmentValidationSchema = z.object({
+  body: z.object({
+    name: z.string({
+      invalid_type_error: 'academic department name is must be required',
+      required_error: 'Name is required',
+    }),
+    academicFaculty: z.string({
+      invalid_type_error: 'Academic Faculty must be string',
+      required_error: 'Name is required',
+    }),
+  }),
+});
+
+export const AcademicDepartmentValidationSchema = {
+  createAcademicDepartmentValidationSchema,
+  updateAcademicDepartmentValidationSchema,
+};
