8d5299549c324af1ae3aff91caaa049ec23a1990
tuhincoder <mdtuhinmahmudul@gmail.com>
2024-12-10 23:56:37 +0600

    global error handle

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,20 +1,26 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextFunction, Request, Response } from 'express';
 
-const globalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  const statusCode = 500;
+import { ErrorRequestHandler } from 'express';
+
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
   const message = err.message || 'something went wrong!';
+  type TErrorSource = {
+    path: string | number;
+    message: string;
+  }[];
+
+  const errorSource: TErrorSource = [
+    {
+      path: '',
+      message: 'something went wrong',
+    },
+  ];
   return res.status(statusCode).json({
     success: false,
     message,
-    error: err,
+    errorSource,
   });
 };
 
