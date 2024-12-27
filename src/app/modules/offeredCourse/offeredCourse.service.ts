973e8e675a3e6453411772816844a90ee86e92e3
tuhincoder <mdtuhinmahmudul@gmail.com>
2024-12-27 15:37:22 +0600

    course faculty schedule complete

--- src/app/modules/offeredCourse/offeredCourse.service.ts
+++ src/app/modules/offeredCourse/offeredCourse.service.ts
@@ -1,13 +1,24 @@
+import QueryBuilder from '../../builder/QueryBuilder';
 import { AcademicDepartment } from '../academicDepartment/academicDepartment.model';
 import { AcademicFaculty } from '../academicFaculty/academicFaculty.model';
 import { Course } from '../course/course.model';
+import { Faculty } from '../Faculty/faculty.model';
 import { SemesterRegistration } from '../semesterRegistration/semesterRegistration.model';
 import { TOfferedCourse } from './offeredCourse.interface';
 import { OfferedCourse } from './offeredCourse.model';
+import { hasTimeConflict } from './offeredCourse.utils';
 //CREATE OFFERED COURSE
 const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
-  const { semesterRegistration, academicFaculty, academicDepartment, course } =
-    payload;
+  const {
+    semesterRegistration,
+    section,
+    academicFaculty,
+    academicDepartment,
+    course,
+    days,
+    startTime,
+    endTime,
+  } = payload;
   // if the semester registration id is exists
   const isSemesterRegistrationExists =
     await SemesterRegistration.findById(semesterRegistration);
@@ -47,34 +58,133 @@ const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
       `This ${isAcademicDepartmentExists.name} is not belong to ${isAcademicFacultyExists.name}`,
     );
   }
+
+  //check if the same offered course same section is same registered semester exists
+  const isSameOfferedCourseExistsWithSameRegisterWithSameSection =
+    await OfferedCourse.findOne({
+      semesterRegistration,
+      course,
+      section,
+    });
+  if (isSameOfferedCourseExistsWithSameRegisterWithSameSection) {
+    throw new Error('offered course with same section is already exists!');
+  }
+  //get the schedules of the faculties
+  const assignSchedules = await OfferedCourse.find({
+    semesterRegistration,
+    days: { $in: days },
+  }).select('days startTime endTime');
+  console.log(assignSchedules);
+
+  const newSchedule = {
+    days,
+    startTime,
+    endTime,
+  };
+  if (hasTimeConflict(assignSchedules, newSchedule)) {
+    throw new Error(
+      'this faculty is not available at the time ! choose other time and date',
+    );
+  }
   const result = await OfferedCourse.create({ ...payload, academicSemester });
   return result;
 };
 
 //GET ALL OFFERED COURSE
 const getAllOfferedCourseFromDB = async (query: Record<string, unknown>) => {
-  const result = await OfferedCourse.find();
+  const offeredCourseQuery = new QueryBuilder(OfferedCourse.find(), query)
+    .filter()
+    .sort()
+    .paginate()
+    .fields();
+  const result = await offeredCourseQuery.modelQuery;
   return result;
 };
 
 //GET SINGLE OFFERED COURSE
 const getSingleOfferedCourseFromDB = async (id: string) => {
-  const result = await OfferedCourse.findById(id);
-  return result;
+  const isSingleOfferedCourse = await OfferedCourse.findById(id);
+  if (!isSingleOfferedCourse) {
+    throw new Error('offered course not found');
+  }
+  return isSingleOfferedCourse;
 };
 
 //UPDATE OFFERED COURSE
 const updateOfferedCourseIntoDB = async (
   id: string,
-  payload: Partial<TOfferedCourse>,
+  payload: Pick<TOfferedCourse, 'days' | 'startTime' | 'endTime'>,
 ) => {
-  const result = await OfferedCourse.findByIdAndUpdate(id);
+  // const {faculty} = payload
+  const { days, startTime, endTime } = payload;
+  const isOfferedCourseExists = await OfferedCourse.findById(id);
+
+  if (!isOfferedCourseExists) {
+    throw new Error('offered course not found');
+  }
+
+  // const isFacultyExists = await Faculty.findById(faculty);
+  // if (!isFacultyExists) {
+  //   throw new Error('Faculty not found!');
+  // }
+
+  //get the schedules of the faculties
+  const semesterRegistration = isOfferedCourseExists.semesterRegistration;
+  const semesterRegistrationStatus =
+    await SemesterRegistration.findById(semesterRegistration);
+  if (semesterRegistrationStatus?.status !== 'UPCOMING') {
+    throw new Error(
+      `you can not update this offered as it is ${semesterRegistrationStatus?.status}`,
+    );
+  }
+  const assignSchedules = await OfferedCourse.find({
+    semesterRegistration,
+    days: { $in: days },
+  }).select('days startTime endTime');
+  console.log(assignSchedules);
+
+  const newSchedule = {
+    days,
+    startTime,
+    endTime,
+  };
+  if (hasTimeConflict(assignSchedules, newSchedule)) {
+    throw new Error(
+      'this faculty is not available at the time ! choose other time and date',
+    );
+  }
+
+  const result = await OfferedCourse.findByIdAndUpdate(id, payload, {
+    new: true,
+  });
   return result;
 };
 
 //DELETE OFFERED COURSE
 const deleteOfferedCourseFromDB = async (id: string) => {
-  const result = await OfferedCourse.findByIdAndUpdate(id);
+  const isOfferedCourseExists = await OfferedCourse.findById(id);
+  if (!isOfferedCourseExists) {
+    throw new Error('offered course not found');
+  }
+
+  //check if the semester registration is upcoming
+  // const semesterRegistration = isOfferedCourseExists.semesterRegistration;
+  // const semesterRegistrationStatus =
+  //   await SemesterRegistration.findById(semesterRegistration).select('status');
+  // if (semesterRegistrationStatus?.status !== 'UPCOMING') {
+  //   throw new Error(
+  //     `Offered course can not update! because the semester is ${semesterRegistrationStatus}`,
+  //   );
+  // }
+  const semesterRegistration = isOfferedCourseExists.semesterRegistration;
+  const semesterRegistrationStatus =
+    await SemesterRegistration.findById(semesterRegistration).select('status');
+  if (semesterRegistrationStatus?.status !== 'UPCOMING') {
+    throw new Error(
+      `Offered course can not update! because the semester is ${semesterRegistration}`,
+    );
+  }
+  const result = await OfferedCourse.findByIdAndDelete(id);
   return result;
 };
 
--- src/app/modules/offeredCourse/offeredCourse.validation.ts
+++ src/app/modules/offeredCourse/offeredCourse.validation.ts
@@ -1,6 +1,16 @@
 import { z } from 'zod';
 import { Days } from './offeredCourse.constant';
 
+const timeStringSchema = z.string().refine(
+  (time) => {
+    const regex = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/; // 00-09 10-19 20-23
+    return regex.test(time);
+  },
+  {
+    message: 'Invalid time format , expected "HH:MM" in 24 hours format',
+  },
+);
+
 const createOfferedCourseValidation = z.object({
   body: z
     .object({
@@ -9,12 +19,12 @@ const createOfferedCourseValidation = z.object({
       academicFaculty: z.string(),
       academicDepartment: z.string(),
       course: z.string(),
-      // faculty: z.string().optional(),
+      // faculty: z.string(),
       maxCapacity: z.number(),
       section: z.number(),
       days: z.array(z.enum([...Days] as [string, ...string[]])),
-      startTime: z.string(),
-      endTime: z.string(),
+      startTime: timeStringSchema,
+      endTime: timeStringSchema,
     })
     .refine(
       (body) => {
@@ -33,19 +43,35 @@ const createOfferedCourseValidation = z.object({
 });
 //update  offered course
 const updateOfferedCourseValidation = z.object({
-  body: z.object({
-    // semesterRegistration: z.string().optional(),
-    // academicSemester: z.string().optional(),
-    // academicFaculty: z.string().optional(),
-    // academicDepartment: z.string().optional(),
-    // course: z.string().optional(),
-    faculty: z.string().optional(),
-    maxCapacity: z.number().optional(),
-    section: z.number().optional(),
-    days: z.enum([...Days] as [string, ...string[]]).optional(),
-    startTime: z.string().optional(),
-    endTime: z.string().optional(),
-  }),
+  body: z
+    .object({
+      // semesterRegistration: z.string(),
+      // academicSemester: z.string(),
+      // academicFaculty: z.string(),
+      // academicDepartment: z.string(),
+      // course: z.string(),
+      faculty: z.string(),
+      maxCapacity: z.number(),
+      section: z.number(),
+      // z.array(z.enum([...Days] as [string, ...string[]])),
+      days: z.array(z.enum([...Days] as [string, ...string[]])),
+      startTime: z.string(),
+      endTime: z.string(),
+    })
+    .refine(
+      (body) => {
+        // startTime : 10:30  => 1970-01-01T10:30
+        //endTime : 12:30  =>  1970-01-01T12:30
+
+        const start = new Date(`1970-01-01T${body.startTime}:00`);
+        const end = new Date(`1970-01-01T${body.endTime}:00`);
+
+        return end > start;
+      },
+      {
+        message: 'Start time should be before End time !  ',
+      },
+    ),
 });
 
 export const OfferedCourseValidations = {
